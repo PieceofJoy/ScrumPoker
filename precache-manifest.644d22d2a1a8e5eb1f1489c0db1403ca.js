ca07d29893cd4dd24a7c80114dec897391f0ac6a
PieceofJoy <pieceofjoyinyourlife@gmail.com>
2020-02-02 14:21:14 -0800

    Updates

---
+++ precache-manifest.644d22d2a1a8e5eb1f1489c0db1403ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c988a486b9b0dd3b5fce593a3ef7a222",
+    "url": "/ScrumPoker/index.html"
+  },
+  {
+    "revision": "9694a09d5ff055dabd2a",
+    "url": "/ScrumPoker/static/css/main.a11c26a6.chunk.css"
+  },
+  {
+    "revision": "2d8c4ec0307c4b6b661f",
+    "url": "/ScrumPoker/static/js/2.a81c2ac1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ScrumPoker/static/js/2.a81c2ac1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9694a09d5ff055dabd2a",
+    "url": "/ScrumPoker/static/js/main.9abdb7b1.chunk.js"
+  },
+  {
+    "revision": "88f7334b4ad7654776c1",
+    "url": "/ScrumPoker/static/js/runtime-main.1ae37de9.js"
+  }
+]);
