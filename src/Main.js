7bf9fb5de6c57234657267cfa816cfce51273110
PieceofJoy <pieceofjoyinyourlife@gmail.com>
2020-02-02 20:57:38 -0500

    Add room modal

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal'; 
+import { Input } from '@material-ui/core';
 import './Main.css';
 
 const Main = () => {
-    return (
-        <div className="main-page">
-            <div>
-                <h1>Scrum Poker</h1>
-            </div>
-            <div className="button-wrapper">
-                <Link to="/room">
-                    <Button variant="contained" color="primary">
-                        Join Room
-                    </Button>
-                </Link>
-                <Button variant="contained" color="primary">
-                    Create Room 
-                </Button>
-            </div>
-        </div>
-    );
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputeValue] = useState('');
+
+  return (
+    <div className="main-page">
+      <div>
+        <h1>Scrum Poker</h1>
+      </div>
+      <div className="button-wrapper">
+        <Button variant="contained" color="primary">
+          Join Room
+        </Button>
+        <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+          Create Room
+        </Button>
+      </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="create-modal"> 
+          Room ID: 
+          <Input value={inputValue} onChange={(e) => setInputeValue(e.target.value)} />
+          <Link to="/room"> 
+          <Button variant="contained" color="primary" onClick={() => setOpen(true)}>  
+          Create Room 
+          </Button>
+          </Link>
+        </div>        
+      </Modal>
+    </div>
+  );
 };
 
 export default Main;
