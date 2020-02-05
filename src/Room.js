a6ba41c3dc8a0209ecc7725b3d2050ae6d9e8327
PieceofJoy <pieceofjoyinyourlife@gmail.com>
2020-02-04 22:04:47 -0500

    Add button and design

--- src/Room.js
+++ src/Room.js
@@ -5,21 +5,43 @@ import "./Room.css";
 
 const Room = () => {
     return (
-        <div>
-            <h2> 
-            Scrum Poker Room
-            </h2> 
-            <h3> 
-                Select a card 
-            </h3>
-            <Link to="/"> 
-                <Button variant="contained" color="primary">
-                    Exit
-                </Button>
-            </Link>
-            <Button variant="contained" color="primary">
-                    Vote 
-                </Button>
+        <div> 
+            <div>
+                <h2>  Scrum Poker Room </h2>
+            </div>
+        
+            <div>
+            <h3> Select a card </h3>
+                <div class="cards-button"> 
+                    <button class="cards"> 0 </button>
+                    <button class="cards"> 1 </button>
+                    <button class="cards"> 2 </button>
+                    <button class="cards"> 3 </button>
+                    <button class="cards"> 5 </button>
+                    <button class="cards"> 8 </button>
+                </div> 
+                <div class="cards-button">
+                    <button class="cards"> 13 </button>
+                    <button class="cards"> 21 </button>
+                    <button class="cards"> 34 </button>
+                    <button class="cards"> 55 </button>
+                    <button class="cards"> 89 </button>
+                    <button class="cards"> ? </button>
+                </div>
+            </div>
+
+            <div> 
+                <Link to="/"> 
+                    <Button variant="contained" color="primary">
+                        Exit
+                    </Button>
+                </Link>
+                
+                    <Button variant="contained" color="primary">
+                            Vote 
+                    </Button>
+                
+            </div>
         </div>
     );
 };
